dd8e547926ae4243d6921bb2ade63fb287b729f4
root <root@apex.sccwrp.org>
2023-06-21 20:51:35 +0000

    Aria- resolved the issue from taxonomy_custom all checks are reevaluated and working properly now

--- proj/custom/taxonomy_custom.py
+++ proj/custom/taxonomy_custom.py
@@ -1,5 +1,5 @@
 # Dont touch this file! This is intended to be a template for implementing new custom checks
-
+from sqlalchemy import create_engine
 from inspect import currentframe
 from flask import current_app, g
 from .functions import checkData, check_multiple_dates_within_site, check_missing_phab_data, check_mismatched_phab_date, consecutive_date
@@ -26,7 +26,7 @@ def taxonomy(all_dfs):
     # we assign dataframes of all_dfs to variables and go from there
     # This is the convention that was followed in the old checker
     
-    # This data type should only have tbl_example
+    # This data typeg should only have tbl_example
     # example = all_dfs['tbl_example']
 
     # Alter this args dictionary as you add checks and use it for the checkData function
@@ -82,32 +82,28 @@ def taxonomy(all_dfs):
     warnings.append(
         checkData(
             'tbl_taxonomysampleinfo', 
-                multiple_dates_within_site_info[0],
+            multiple_dates_within_site_info[0],
             'sampledate',
             'Value Error', 
             f'Warning! You are submitting taxonomy data with multiple dates for the same site. {multiple_dates_within_site_info[1]} unique sample dates were submitted. Is this correct?'
         )
     )    
-    
+    # Check 2: Within taxonomy data, return a warning if a submission contains multiple dates within a single site
     warnings.append(
         checkData(
             'tbl_taxonomyresults', 
-                multiple_dates_within_site_results[0],
+            multiple_dates_within_site_results[0],
             'sampledate',
             'Value Error', 
             f'Warning! You are submitting taxonomy data with multiple dates for the same site. {multiple_dates_within_site_results[1]} unique sample dates were submitted. Is this correct?'
         )
     )  
-    #multiple_dates_within_site_results[1]
 
-    # Check 2: Return warnings on missing phab data
-    # Ayah: commented out logic check to be able to work on the other custom checks
+    # Check 3: Return warnings on missing phab data
     #phab data that will be used in checks 2 and 3 below
     info_sites = list(set(taxonomysampleinfo['stationcode'].unique()))
     results_sites = list(set(taxonomyresults['stationcode'].unique()))
     #assert info_sites == results_sites, "unique stationcodes do not match between sampleinfo and results dataframes"
-
-
     sql_query = f"""
         SELECT DISTINCT STATIONCODE,
 	    SAMPLEDATE
@@ -116,59 +112,80 @@ def taxonomy(all_dfs):
 	    AND STATIONCODE in ('{"','".join(info_sites)}')
         ;"""
     phab_data = pd.read_sql(sql_query, g.eng)
+    print('phab_data')
+    print(phab_data)
     # end logic check
 
     test_phab = pd.DataFrame({'stationcode' : ['410M01628', 'SMC01972'], 'sampledate': ['2018-06-28 00:00:00', '2010-06-07 00:00:00']})
     test_phab['sampledate'] = pd.to_datetime(test_phab['sampledate'])
 
-    missing_phab_data_info = check_missing_phab_data(taxonomysampleinfo, phab_data)
-    missing_phab_data_results = check_missing_phab_data(taxonomyresults, phab_data)
+    # Check - missing phab records for taxonomysampleinfo submission
 
+    ##not using check_missing_phab_data function since multiple sampledates for some stationcodes
+    ##instead, we will just write the check directly in custom file
+
+    #Merging to two tables on stationcode and sampledate. 
+    #Adding "indicator = True" adds a column called _merge that tells you whether the row is in both or one of the tables.
+    merge_tax_with_phab = taxonomysampleinfo.merge(phab_data, how = 'left', on = ['stationcode', 'sampledate'], indicator = True)
+    
+    #Printing the rows that do not match Taxonomy and phab. 
+    badrows_sampleinfo = merge_tax_with_phab[merge_tax_with_phab['_merge'] == 'left_only'].tmp_row.tolist()
+    
     warnings.append(
         checkData(
             'tbl_taxonomysampleinfo', 
-            missing_phab_data_info[0],
+            badrows_sampleinfo,
             'sampledate',
             'Value Error', 
-            f'Warning! PHAB data has not been submitted for site(s) {", ".join(missing_phab_data_info[1])}. If PHAB data are available, please submit those data before submitting taxonomy data.'
+            f'Warning! PHAB data has not been submitted for site(s): {list(taxonomysampleinfo.stationcode[badrows_sampleinfo])}. If PHAB data are available, please submit those data before submitting taxonomy data.'
         )
     )  
+
+   # Check 4- missing phab records for taxonomyresults submission
+
+    #Adding "indicator = True" adds a column called _merge that tells you whether the row is in both or one of the tables.
+    merge_tax_with_phab = taxonomyresults.merge(phab_data, how = 'left', on = ['stationcode', 'sampledate'], indicator = True)
+    #keeping record of the rows that are missing in phab but are present in taxonomy . 
+    badrows_results = merge_tax_with_phab[merge_tax_with_phab['_merge'] == 'left_only'].tmp_row.tolist()
 
     warnings.append(
         checkData(
             'tbl_taxonomyresults', 
-            missing_phab_data_results[0],
+            badrows_results,
             'sampledate',
             'Value Error', 
-            f'Warning! PHAB data has not been submitted for site(s) {", ".join(missing_phab_data_results[1])}. If PHAB data are available, please submit those data before submitting taxonomy data.'
+            f'Warning! PHAB data has not been submitted for site(s): {list(taxonomyresults.stationcode[badrows_results])}. If PHAB data are available, please submit those data before submitting taxonomy data.'
         )
     )  
 
-    # # Check 3: Return warnings on submission dates mismatching with phab dates
-    # mismatched_phab_date_info = check_mismatched_phab_date(taxonomysampleinfo, phab_data)
-    # mismatched_phab_date_results = check_mismatched_phab_date(taxonomyresults, phab_data)
+    ## Check 5: Return warnings on submission dates mismatching with phab dates
+    mismatched_phab_date_info = check_mismatched_phab_date(taxonomysampleinfo, phab_data)    
 
-    # warnings.append(
-    #     checkData(
-    #         'tbl_taxonomysampleinfo', 
-    #         mismatched_phab_date_info[0],
-    #         'sampledate',
-    #         'Value Error', 
-    #         f'Warning! PHAB was sampled on {", ".join(mismatched_phab_date_info[1])}. Sample date for PHAB data for this site and year does not match the sample date in this submission. Please verify that both dates are correct. If submitted data requires correction, please contact Jeff Brown at jeffb@sccwrp.org.'
-    #     )
-    # )  
+    mismatched_phab_date_results = check_mismatched_phab_date(taxonomyresults, phab_data)
 
-    # warnings.append(
-    #     checkData(
-    #         'tbl_taxonomyresults', 
-    #         mismatched_phab_date_results[0],
-    #         'sampledate',
-    #         'Value Error', 
-    #         f'Warning! PHAB was sampled on {", ".join(mismatched_phab_date_results[1])}. Sample date for PHAB data for this site and year does not match the sample date in this submission. Please verify that both dates are correct. If submitted data requires correction, please contact Jeff Brown at jeffb@sccwrp.org.'
-    #     )
-    # ) 
+    #sampleinfo sheet
+    warnings.append(
+        checkData(
+            'tbl_taxonomysampleinfo', 
+            mismatched_phab_date_info[0],
+            'sampledate',
+            'Value Error', 
+            f'Warning! PHAB was sampled on {", ".join(mismatched_phab_date_info[1])}. Sample date for PHAB data for this site and year does not match the sample date in this submission. Please verify that both dates are correct. If submitted data requires correction, please contact Jeff Brown at jeffb@sccwrp.org.'
+        )
+    )  
 
-    # Check:TaxonomicQualifier must have at least one TaxonomicQualifier from lu_taxonomicqualifier 
+    #results sheet
+    warnings.append(
+        checkData(
+            'tbl_taxonomyresults', 
+            mismatched_phab_date_results[0],
+            'sampledate',
+            'Value Error', 
+            f'Warning! PHAB was sampled on {", ".join(mismatched_phab_date_results[1])}. Sample date for PHAB data for this site and year does not match the sample date in this submission. Please verify that both dates are correct. If submitted data requires correction, please contact Jeff Brown at jeffb@sccwrp.org.'
+        )
+    ) 
+
+    # Check 6:TaxonomicQualifier must have at least one TaxonomicQualifier from lu_taxonomicqualifier 
     errs.append(
         checkData(
             'tbl_taxonomyresults', 
@@ -179,7 +196,8 @@ def taxonomy(all_dfs):
         )
     ) 
     print('##############the code ran here########') 
-    # Check: Error on consecutive dates to make sure user did not drag down date for SampleDate for tbl_taxonomysampleinfo #done
+    # Check 7: Error on consecutive dates to make sure user did not drag down date for SampleDate for tbl_taxonomysampleinfo #done
+    #warnning
     errs.append(
         checkData(
             'tbl_taxonomysampleinfo', 
@@ -189,8 +207,8 @@ def taxonomy(all_dfs):
             'Consecutive Dates. Make sure you did not accidentally drag down the date'
         )
     )  
-    #Check: Error on consecutive dates to make sure user did not drag down date for SampleDate for tbl_taxonomyresults #done
-
+    #Check 8: Error on consecutive dates to make sure user did not drag down date for SampleDate for tbl_taxonomyresults #done
+    #warnning
     errs.append(
         checkData(
             'tbl_taxonomyresults',
@@ -200,5 +218,23 @@ def taxonomy(all_dfs):
             'Consecutive Dates. Make sure you did not accidentally drag down the date'
         )
     ) 
-   
+    print('code ran consecutive date check')
+
+    #Check 9: FinalID / LifeStageCode combination must match combination found in vw_organism_lifestage_lookup
+    lu_organisms = pd.read_sql("SELECT concat(finalid, '_', lifestagecode) as combinations FROM vw_organism_lifestage_lookup;", g.eng)
+    sql_combos = lu_organisms['combinations'].tolist()
+    taxonomyresults['tmp_comb'] = taxonomyresults['finalid'].astype(str) + '_' + taxonomyresults['lifestagecode'].astype(str)
+    badrows = taxonomyresults[~taxonomyresults['tmp_comb'].isin(sql_combos)].tmp_row.tolist()
+
+    errs.append(
+        checkData(
+            'tbl_taxonomyresults',
+            badrows,
+            #taxonomyresults[~pd.Series(taxonomyresults.finalid + '_' + taxonomyresults.lifestagecode).isin(valid_pairs_list)].tmp_row.tolist(),
+            'finalid, lifestagecode',
+            'Undefined Error',
+            'FinalID/LifeStageCode combination must match the combination found in vw_organism_lifestage_lookup'
+            )
+    )
+
     return {'errors': errs, 'warnings': warnings}
