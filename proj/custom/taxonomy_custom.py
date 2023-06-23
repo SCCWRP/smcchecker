4832590fccf19347efd9931512dfff262f2f865d
root <root@apex.sccwrp.org>
2023-06-23 00:11:31 +0000

    Aria - analysis R script is added and working

--- proj/custom/taxonomy_custom.py
+++ proj/custom/taxonomy_custom.py
@@ -1,10 +1,11 @@
 # Dont touch this file! This is intended to be a template for implementing new custom checks
-
+from sqlalchemy import create_engine
 from inspect import currentframe
-from flask import current_app, g
+from flask import current_app, session, g
 from .functions import checkData, check_multiple_dates_within_site, check_missing_phab_data, check_mismatched_phab_date, consecutive_date
 import pandas as pd
-import geopy
+import re, os
+import subprocess as sp
 
 def taxonomy(all_dfs):
     
@@ -27,7 +28,7 @@ def taxonomy(all_dfs):
     # we assign dataframes of all_dfs to variables and go from there
     # This is the convention that was followed in the old checker
     
-    # This data type should only have tbl_example
+    # This data typeg should only have tbl_example
     # example = all_dfs['tbl_example']
 
     # Alter this args dictionary as you add checks and use it for the checkData function
@@ -76,67 +77,117 @@ def taxonomy(all_dfs):
         "error_message": ""
     }
 
-    #IMPORTANT UPDATE
-    #NOTE: Aria- I changed check 1 and 2 to warnings with roberts approval since it made more sense as a warning. Also to clarify this-
-    # -check is checking if the entire column of sampledate is consecutive meaning that if just one part is consecutive it will not come 
-    # out as an warning the entire column must be consecutive for it to be a warning!
-
-    # Check 1: Error on consecutive dates to make sure user did not drag down date for SampleDate for tbl_taxonomysampleinfo #done
-    warnings.append(
-        checkData(
-            'tbl_taxonomysampleinfo', 
-            consecutive_date(taxonomysampleinfo),
-            'sampledate',
-            'Error', 
-            'SampleDate has been duplicated in sampleinfo sheet, make sure no date has been dragged down for that column'
-        )
-    )  
-    #Check 2: Error on consecutive dates to make sure user did not drag down date for SampleDate for tbl_taxonomyresults #done
-    warnings.append(
-        checkData(
-            'tbl_taxonomyresults', 
-            consecutive_date(taxonomyresults),
-            'sampledate',
-            'Error', 
-            'SampleDate has been duplicated in results sheet, make sure no date has been dragged down for that column'
-        )
-    ) 
-    # warnings.append(
-    #     checkData(
-    #         'tbl_taxonomyresults',
-    #         consecutive_date(taxonomyresults),
-    #         'sampledate',
-    #         'Error', 
-    #         'SampleDate has been duplicated in result sheet, make sure no date has been dragged down for that column'
-    #     )
-    # )
-
-    # Check 3: Within taxonomy data, return a warning if a submission contains multiple dates within a single site for tbl_taxonomysampleinfo      
+    # Check 1: Within taxonomy data, return a warning if a submission contains multiple dates within a single site    
     multiple_dates_within_site_info = check_multiple_dates_within_site(taxonomysampleinfo)
     multiple_dates_within_site_results = check_multiple_dates_within_site(taxonomyresults)
 
     warnings.append(
         checkData(
             'tbl_taxonomysampleinfo', 
-                multiple_dates_within_site_info[0],
+            multiple_dates_within_site_info[0],
             'sampledate',
             'Value Error', 
             f'Warning! You are submitting taxonomy data with multiple dates for the same site. {multiple_dates_within_site_info[1]} unique sample dates were submitted. Is this correct?'
         )
     )    
-    
-    # Check 4: Within taxonomy data, return a warning if  a submission contains multiple dates within a single site for tbl_taxnomyresults    
+    # Check 2: Within taxonomy data, return a warning if a submission contains multiple dates within a single site
     warnings.append(
         checkData(
             'tbl_taxonomyresults', 
-                multiple_dates_within_site_results[0],
+            multiple_dates_within_site_results[0],
             'sampledate',
             'Value Error', 
             f'Warning! You are submitting taxonomy data with multiple dates for the same site. {multiple_dates_within_site_results[1]} unique sample dates were submitted. Is this correct?'
         )
     )  
 
-    # Check 5:TaxonomicQualifier must have at least one TaxonomicQualifier from lu_taxonomicqualifier 
+    # Check 3: Return warnings on missing phab data
+    #phab data that will be used in checks 2 and 3 below
+    info_sites = list(set(taxonomysampleinfo['stationcode'].unique()))
+    results_sites = list(set(taxonomyresults['stationcode'].unique()))
+    #assert info_sites == results_sites, "unique stationcodes do not match between sampleinfo and results dataframes"
+    sql_query = f"""
+        SELECT DISTINCT STATIONCODE,
+	    SAMPLEDATE
+        FROM UNIFIED_PHAB
+        WHERE RECORD_ORIGIN = 'SMC'
+	    AND STATIONCODE in ('{"','".join(info_sites)}')
+        ;"""
+    phab_data = pd.read_sql(sql_query, g.eng)
+    print('phab_data')
+    print(phab_data)
+    # end logic check
+
+    test_phab = pd.DataFrame({'stationcode' : ['410M01628', 'SMC01972'], 'sampledate': ['2018-06-28 00:00:00', '2010-06-07 00:00:00']})
+    test_phab['sampledate'] = pd.to_datetime(test_phab['sampledate'])
+
+    # Check - missing phab records for taxonomysampleinfo submission
+
+    ##not using check_missing_phab_data function since multiple sampledates for some stationcodes
+    ##instead, we will just write the check directly in custom file
+
+    #Merging to two tables on stationcode and sampledate. 
+    #Adding "indicator = True" adds a column called _merge that tells you whether the row is in both or one of the tables.
+    merge_tax_with_phab = taxonomysampleinfo.merge(phab_data, how = 'left', on = ['stationcode', 'sampledate'], indicator = True)
+    
+    #Printing the rows that do not match Taxonomy and phab. 
+    badrows_sampleinfo = merge_tax_with_phab[merge_tax_with_phab['_merge'] == 'left_only'].tmp_row.tolist()
+    
+    warnings.append(
+        checkData(
+            'tbl_taxonomysampleinfo', 
+            badrows_sampleinfo,
+            'sampledate',
+            'Value Error', 
+            f'Warning! PHAB data has not been submitted for site(s): {list(taxonomysampleinfo.stationcode[badrows_sampleinfo])}. If PHAB data are available, please submit those data before submitting taxonomy data.'
+        )
+    )  
+
+   # Check 4- missing phab records for taxonomyresults submission
+
+    #Adding "indicator = True" adds a column called _merge that tells you whether the row is in both or one of the tables.
+    merge_tax_with_phab = taxonomyresults.merge(phab_data, how = 'left', on = ['stationcode', 'sampledate'], indicator = True)
+    #keeping record of the rows that are missing in phab but are present in taxonomy . 
+    badrows_results = merge_tax_with_phab[merge_tax_with_phab['_merge'] == 'left_only'].tmp_row.tolist()
+
+    warnings.append(
+        checkData(
+            'tbl_taxonomyresults', 
+            badrows_results,
+            'sampledate',
+            'Value Error', 
+            f'Warning! PHAB data has not been submitted for site(s): {list(taxonomyresults.stationcode[badrows_results])}. If PHAB data are available, please submit those data before submitting taxonomy data.'
+        )
+    )  
+
+    ## Check 5: Return warnings on submission dates mismatching with phab dates
+    mismatched_phab_date_info = check_mismatched_phab_date(taxonomysampleinfo, phab_data)    
+
+    mismatched_phab_date_results = check_mismatched_phab_date(taxonomyresults, phab_data)
+
+    #sampleinfo sheet
+    warnings.append(
+        checkData(
+            'tbl_taxonomysampleinfo', 
+            mismatched_phab_date_info[0],
+            'sampledate',
+            'Value Error', 
+            f'Warning! PHAB was sampled on {", ".join(mismatched_phab_date_info[1])}. Sample date for PHAB data for this site and year does not match the sample date in this submission. Please verify that both dates are correct. If submitted data requires correction, please contact Jeff Brown at jeffb@sccwrp.org.'
+        )
+    )  
+
+    #results sheet
+    warnings.append(
+        checkData(
+            'tbl_taxonomyresults', 
+            mismatched_phab_date_results[0],
+            'sampledate',
+            'Value Error', 
+            f'Warning! PHAB was sampled on {", ".join(mismatched_phab_date_results[1])}. Sample date for PHAB data for this site and year does not match the sample date in this submission. Please verify that both dates are correct. If submitted data requires correction, please contact Jeff Brown at jeffb@sccwrp.org.'
+        )
+    ) 
+
+    # Check 6:TaxonomicQualifier must have at least one TaxonomicQualifier from lu_taxonomicqualifier 
     errs.append(
         checkData(
             'tbl_taxonomyresults', 
@@ -146,8 +197,32 @@ def taxonomy(all_dfs):
             'Taxonomicqualifier must contain at least one value from lu_taxonomicqualifier '
         )
     ) 
+    print('##############the code ran here########') 
+    # Check 7: Error on consecutive dates to make sure user did not drag down date for SampleDate for tbl_taxonomysampleinfo #done
+    #warnning
+    errs.append(
+        checkData(
+            'tbl_taxonomysampleinfo', 
+            consecutive_date(taxonomysampleinfo),
+            'sampledate',
+            'Error', 
+            'Consecutive Dates. Make sure you did not accidentally drag down the date'
+        )
+    )  
+    #Check 8: Error on consecutive dates to make sure user did not drag down date for SampleDate for tbl_taxonomyresults #done
+    #warnning
+    errs.append(
+        checkData(
+            'tbl_taxonomyresults',
+            consecutive_date(taxonomyresults),
+            'sampledate',
+            'Error', 
+            'Consecutive Dates. Make sure you did not accidentally drag down the date'
+        )
+    ) 
+    print('code ran consecutive date check')
 
-    # Check 6:FinalID/LifeStageCode combination must match the combination found in vw_organism_lifestage_lookup
+    #Check 9: FinalID / LifeStageCode combination must match combination found in vw_organism_lifestage_lookup
     lu_organisms = pd.read_sql("SELECT concat(finalid, '_', lifestagecode) as combinations FROM vw_organism_lifestage_lookup;", g.eng)
     sql_combos = lu_organisms['combinations'].tolist()
     taxonomyresults['tmp_comb'] = taxonomyresults['finalid'].astype(str) + '_' + taxonomyresults['lifestagecode'].astype(str)
@@ -164,100 +239,93 @@ def taxonomy(all_dfs):
             )
     )
 
-    # Check 7-8: Return warnings on missing phab data
-    # Ayah: commented out logic check to be able to work on the other custom checks
-    #phab data that will be used in checks 7-8 below
-    info_sites = list(set(taxonomysampleinfo['stationcode'].unique()))
-    results_sites = list(set(taxonomyresults['stationcode'].unique()))
-    #assert info_sites == results_sites, "unique stationcodes do not match between sampleinfo and results dataframes"
-    sql_query = f"""
-        SELECT DISTINCT STATIONCODE,
-	    SAMPLEDATE
-        FROM UNIFIED_PHAB
-        WHERE RECORD_ORIGIN = 'SMC'
-	    AND STATIONCODE in ('{"','".join(info_sites)}')
-        ;"""
-    phab_data = pd.read_sql(sql_query, g.eng)
-    test_phab = pd.DataFrame({'stationcode' : ['410M01628', 'SMC01972'], 'sampledate': ['2018-06-28 00:00:00', '2010-06-07 00:00:00']})
-    test_phab['sampledate'] = pd.to_datetime(test_phab['sampledate'])
+    
 
-    missing_phab_data_info = check_missing_phab_data(taxonomysampleinfo, phab_data)
-    missing_phab_data_results = check_missing_phab_data(taxonomyresults, phab_data)
+    print("-------------------------------------------------------- R SCRIPT -------------------------------------------")
+    print("Errors list")
+    print(errs)
 
-    #check 7: Warn if PHAB sampleinfo data has not been submitted for StationCode/SampleDate prior to Taxonomy submission
-    warnings.append(
-        checkData(
-            'tbl_taxonomysampleinfo', 
-            missing_phab_data_info[0],
-            'sampledate',
-            'Value Error', 
-            f'Warning! PHAB data has not been submitted for site(s) {", ".join(missing_phab_data_info[1])}. If PHAB data are available, please submit those data before submitting taxonomy data.'
-        )
-    )  
+    # if all(not err for err in errs):
+    #     print("No errors: errs list is empty")
+    # else:
+    #     print("errs list is not empty")
 
-    #check 8: Warn if PHAB results data has not been submitted for StationCode/SampleDate prior to Taxonomy submission
-    warnings.append(
-        checkData(
-            'tbl_taxonomyresults', 
-            missing_phab_data_results[0],
-            'sampledate',
-            'Value Error', 
-            f'Warning! PHAB data has not been submitted for site(s) {", ".join(missing_phab_data_results[1])}. If PHAB data are available, please submit those data before submitting taxonomy data.'
-        )
-    )  
+    if all(not err for err in errs):
+        print("No errors - running analysis routine")
+        # Rscript /path/demo.R tmp.csv
+        print("session.get('excel_path')")
+        print(session.get('excel_path'))
+        print("os.path.join(os.getcwd(), 'R', 'csci.R')")
+        print(os.path.join(os.getcwd(), 'R', 'csci.R'))
+        cmdlist = [
+            'Rscript',
+            f"{os.path.join(os.getcwd(),'R', 'csci.R')}",
+            f"{session.get('submission_dir')}",
+            'output.csv'
+        ]
+        print(cmdlist)
+        proc = sp.run(cmdlist, stdout=sp.PIPE, stderr=sp.PIPE, universal_newlines = True)
 
-    # # Check 9-10: Return warnings on submission dates mismatching with phab dates
-    mismatched_phab_date_info = check_mismatched_phab_date(taxonomysampleinfo, phab_data)
-    mismatched_phab_date_results = check_mismatched_phab_date(taxonomyresults, phab_data)
+        if not bool(re.search(proc.stderr, '\s*')):
+            print(f"Error occurred in OA analysis script:\n{proc.stderr}")
 
-    #check 9: Warn if Taxonomy submission dates mismatch with PHAB dates
-    warnings.append(
-        checkData(
-            'tbl_taxonomysampleinfo', 
-            mismatched_phab_date_info[0],
-            'sampledate',
-            'Value Error', 
-            f'Warning! PHAB was sampled on {", ".join(mismatched_phab_date_info[1])}. Sample date for PHAB data for this site and year does not match the sample date in this submission. Please verify that both dates are correct. If submitted data requires correction, please contact Jeff Brown at jeffb@sccwrp.org.'
-        )
-    )  
+        # submission_dir = os.path.join(os.getcwd(), 'R', 'submission_dir')
+        ctdpath = os.path.join(session.get('submission_dir'),'output.csv')
+        print(ctdpath)
+        print("after printing ctdpath")
 
-    warnings.append(
-        checkData(
-            'tbl_taxonomyresults', 
-            mismatched_phab_date_results[0],
-            'sampledate',
-            'Value Error', 
-            f'Warning! PHAB was sampled on {", ".join(mismatched_phab_date_results[1])}. Sample date for PHAB data for this site and year does not match the sample date in this submission. Please verify that both dates are correct. If submitted data requires correction, please contact Jeff Brown at jeffb@sccwrp.org.'
-        )
-    ) 
-    print('Aria This is the Phab Data:------------------------------------------------------------------------------------')
-    print(phab_data)
-
-## LOGIC CHECK????
-
-# # Check 10: All records for each table must have a corresponding record in the other table due on submission. Join tables on StationCode/SampleDate/FieldReplicate
-# missing_records_info = taxonomysampleinfo[~taxonomysampleinfo[['stationcode', 'sampledate', 'fieldreplicate']].apply(tuple, axis=1).isin(taxonomyresults[['stationcode', 'sampledate', 'fieldreplicate']].apply(tuple, axis=1))].tmp_row.tolist()
-# missing_records_results = taxonomyresults[~taxonomyresults[['stationcode', 'sampledate', 'fieldreplicate']].apply(tuple, axis=1).isin(taxonomysampleinfo[['stationcode', 'sampledate', 'fieldreplicate']].apply(tuple, axis=1))].tmp_row.tolist()
-
-#     errs.append(
-#         checkData(
-#             'tbl_taxonomysampleinfo',
-#             missing_records_info,
-#             'stationcode, sampledate, fieldreplicate',
-#             'Value Error',
-#             'Some records in tbl_taxonomysampleinfo do not have corresponding records in tbl_taxonomyresults for the same StationCode/SampleDate/FieldReplicate.'
-#         )
-#     )
-
-#     errs.append(
-#         checkData(
-#             'tbl_taxonomyresults',
-#             missing_records_results,
-#             'stationcode, sampledate, fieldreplicate',
-#             'Value Error',
-#             'Some records in tbl_taxonomyresults do not have corresponding records in tbl_taxonomysampleinfo for the same StationCode/SampleDate/FieldReplicate.'
-#         )
-#     )
+        # if proc == 0:
+        #     print("R script executed successfully.")
+        # else:
+        #     print("Error: Failed to execute the R script")
 
 
+      # open an ExcelWriter object to append to the excel workbook
+        writer = pd.ExcelWriter(session.get('excel_path'), engine = 'openpyxl', mode = 'a')
+        
+        if os.path.exists(ctdpath):
+            ctd = pd.read_csv(ctdpath)
+            ctd.to_excel(writer, sheet_name = 'analysis_csci_placeholder', index = False)
+
+        else:
+            if not os.path.exists(ctdpath):
+                print("OA Analysis ran with no errors, but the CTD analysis csv file was not found")
+
+            warnings.append(checkData('tbl_oactd', ctd.tmp_row.tolist(), 'Season,Agency,SampleDate,SampleTime,Station,Depth,FieldRep,LabRep','Undefined Warning', 'Could not process analysis for this data set'))
+            
+        
+        writer.close()
+
+    else:
+        print("Errors found. Skipping the analysis routine.")
+
+############################################################################################################################
+#### old code to default if Rscript breaks ##################
+    # if all(not err for err in errs):
+    #     print("No errors - running analysis routine")
+    #     # Rscript /path/demo.R tmp.csv
+    #     print("session.get('excel_path')")
+    #     print(session.get('excel_path'))
+    #     print("os.path.join(os.getcwd(), 'R', 'csci.R')")
+    #     print(os.path.join(os.getcwd(), 'R', 'csci.R'))
+    #     cmdlist = [
+    #         'Rscript',
+    #         f"{os.path.join(os.getcwd(),'R', 'csci.R')}",
+    #         f"{session.get('submission_dir')}",
+    #         'output.csv'
+    #     ]
+    #     print(cmdlist)
+        
+    #     proc = sp.run(cmdlist)
+
+    #     submission_dir = os.path.join(os.getcwd(), 'R', 'submission_dir')
+    #     ctdpath = os.path.join(session.get('submission_dir'),'output.csv')
+    #     print(ctdpath)
+    #     print("after printing ctdpath")
+
+    # else:
+    #     print("Errors found. Skipping the analysis routine.")
+#################################################################################################################
+
+    print("Before return statement------------------------------------------------------")
     return {'errors': errs, 'warnings': warnings}
