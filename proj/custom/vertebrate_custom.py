aac34a0e8637278e5a7e8ac6a3e809e9cdd4512e
Debian <admin@apex.sccwrp.org>
2023-10-06 00:30:06 +0000

    added lu_list links to vertebrate error message for check 1

--- proj/custom/vertebrate_custom.py
+++ proj/custom/vertebrate_custom.py
@@ -78,11 +78,11 @@ def vertebrate(all_dfs):
     print("# CHECK - 1")
     # Description: Check If no_observation == F (false) then taxon, lifestage, abundance fields are required and cant be 'Not Recorded', and they must come from lu_vertebratetaxon, lu_vertebratelifestage, and lu_vertebrateabundance respectively (🛑 ERROR 🛑)
     # Created Coder: Aria Askaryar
-    # Created Date: 5/11/2023
-    # Last Edited Date: 08/29/23
-    # Last Edited Coder: Aria Askaryar
+    # Created Date: 10/2/2023
+    # Last Edited Date: 10/5/2023
+    # Last Edited Coder: Ayah Halabi
     # NOTE (08/29/23): Aria adjusts the format so it follows the coding standard. works   
-
+    # NOTE (10/5/2023): Ayah added links for lu_lists
     #Taxon
     errs.append(
         checkData(
@@ -90,7 +90,7 @@ def vertebrate(all_dfs):
             vertebrateobservation[((vertebrateobservation['no_observation'] == "F") & (vertebrateobservation['taxon'] == 'Not Recorded'))].tmp_row.tolist(),
             'taxon',
             'undefined error',
-            'If no_observation = F then taxon fields is required, taxon cant be empty or null '
+            'If no_observation = F then taxon fields is required, taxon cant be Not Recorded. For more information, you may refer to the  <a href=scraper?action=help&layer=lu_vertebratetaxon>lu_vertebratetaxon</a>">lu_vertebratetaxon</a> '
             )
     )
 
@@ -101,7 +101,7 @@ def vertebrate(all_dfs):
             vertebrateobservation[((vertebrateobservation['no_observation'] == "F") & (vertebrateobservation['lifestage'] == 'Not Recorded'))].tmp_row.tolist(),
             'lifestage',
             'undefined error',
-            'If no_observation = F then lifestage fields is required, lifestage cant be empty or null '
+            'If no_observation = F then lifestage fields is required, lifestage cant be Not Recorded.For more information, you may refer to the  <a href=scraper?action=help&layer=lu_vertebratetaxon>lu_vertebratelifestage</a>">lu_vertebratelifestage</a> '
             )
     )
 
@@ -112,7 +112,7 @@ def vertebrate(all_dfs):
             vertebrateobservation[((vertebrateobservation['no_observation'] == "F") & (vertebrateobservation['abundance'] == 'Not Recorded'))].tmp_row.tolist(),
             'abundance',
             'undefined error',
-            'If no_observation = F then abundance fields is required, abundance cant be empty or null '
+            'If no_observation = F then abundance fields is required, abundance cant be Not Recorded.For more information, you may refer to the  <a href=scraper?action=help&layer=lu_vertebrabundance>lu_vertebrateabundance</a>">lu_vertebrateabundance</a> '
             )
     )
    # END OF CHECK - If No_Observation is False, then Taxon is required with value from lu_vertebratetaxon. (🛑 ERROR 🛑)
