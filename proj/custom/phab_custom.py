f6e6c2657d509ba7c50326e8b8c6f922aaaab019
Debian <admin@apex.sccwrp.org>
2023-10-17 19:52:51 +0000

    Aria Askaryar -10/17/2023 Wrote and QA'ed checks 1 and 2 for phab

--- proj/custom/phab_custom.py
+++ proj/custom/phab_custom.py
@@ -75,42 +75,76 @@ def phab(all_dfs):
     # ------------------------------------------------ PHAB Checks ----------------------------------------------------- #
     # ------------------------------------------------------------------------------------------------------------------ #
     ######################################################################################################################
-    # eng = create_engine(os.environ.get('DB_CONNECTION_STRING'))
-    # lu_stations = pd.read_sql("select stationid, latitude, longitude from lu_stations",eng)
-    # print("lu_stations: \n")
-    # print(lu_stations)
+    #this is calling lu_stations from database into lu_stations df
+    eng = create_engine(os.environ.get('DB_CONNECTION_STRING'))
+    lu_stations = pd.read_sql("select stationid, latitude, longitude from lu_stations",eng)
+    print("lu_stations: \n")
+    print(lu_stations)
 
+    ## this custom function will check the total distance meaning both latitude and longitude combined
     # def calculate_distance(row):
     #     # Calculate distance using geopy considering both latitude and longitude
     #     coords_db = (row['latitude'], row['longitude'])  # from database
     #     coords_df = (row['actual_latitude'], row['actual_longitude'])  # from excel
     #     return geopy.distance.distance(coords_db, coords_df).m
+
+    def calculate_latitude_distance(row):
+        # Calculate distance using geopy considering only latitude
+        coords_db = (row['latitude'], 0)  # from database 
+        coords_df = (row['actual_latitude'], 0)  # from excel 
+        return geopy.distance.distance(coords_db, coords_df).m
+
+    def calculate_longitude_distance(row):
+        # Calculate distance using geopy considering only longitude
+        coords_db = (0, row['longitude'])  # from database 
+        coords_df = (0, row['actual_longitude'])  # from excel
+        return geopy.distance.distance(coords_db, coords_df).m
     
-    # # Merge based on stationid from lu_stations and stationcode from phab
-    # merged_df = pd.merge(lu_stations, phab, left_on='stationid', right_on='stationcode', how='inner')
-    # merged_df['distance'] = merged_df.apply(calculate_distance, axis=1)
-    # print(merged_df)
+    # Merge based on stationid from lu_stations and stationcode from phab
+    merged_df = pd.merge(lu_stations, phab, left_on='stationid', right_on='stationcode', how='inner')
+    merged_df['latitude_difference'] = merged_df.apply(calculate_latitude_distance, axis=1)
+    merged_df['longitude_difference'] = merged_df.apply(calculate_longitude_distance, axis=1)
+    distance_threshold = 300  # 300 meters
     
     print("# CHECK - 1")
     # Description: Actual latitude should not be 300m away from target latitude.(🛑 Warning 🛑)
     # Created Coder: Aria Askaryar
     # Created Date: 10/16/2023
-    # Last Edited Date: 10/16/2023
+    # Last Edited Date: 10/17/2023
     # Last Edited Coder: Aria Askaryar
-    # NOTE (08/22/23): Aria created the check and ran it through the QA process
-    # distance_threshold = 300  # 300 meters
-    # errs.append(
-    #     checkData(
-    #         'tbl_phab',
-    #         merged_df[merged_df['distance'] > distance_threshold].index.tolist(),
-    #         'actual_latitude',
-    #         'Undefined Error',
-    #         'Actual latitude should not be more than 300m away from target latitude coordinates.'                  
-    #     )
-    # )
+    # NOTE (10/16/2023): Aria created the check
+    # NOTE (10/17/2023): Aria ran it through the QA process
+    errs.append(
+        checkData(
+            'tbl_phab',
+            merged_df[merged_df['latitude_difference'] > distance_threshold].index.tolist(),
+            'actual_latitude',
+            'Undefined Error',
+            'Actual latitude should not be more than 300 meters away from target latitude coordinates.'                  
+        )
+    )
     # END OF CHECK - Actual latitude should not be 300m away from target latitude. (🛑 Warning 🛑)
-    print("# END OF CHECK - 1")
+    print("# END OF CHECK - 1") 
 
+    print("# CHECK - 2")
+    # Description: Actual longitude should not be 300m away from target longitude (🛑 Warning 🛑)
+    # Created Coder: Aria Askaryar
+    # Created Date: 10/17/2023
+    # Last Edited Date: 10/17/2023
+    # Last Edited Coder: Aria Askaryar
+    # NOTE (10/16/2023): Aria created the check
+    # NOTE (10/17/2023): Aria ran it through the QA process
+    errs.append(
+        checkData(
+            'tbl_phab',
+            merged_df[merged_df['longitude_difference'] > distance_threshold].index.tolist(),
+            'actual_longitude',
+            'Undefined Error',
+            'Actual longitude should not be 300m away from target longitude.'                  
+        )
+    )
+    # END OF CHECK - Actual longitude should not be 300m away from target longitude (🛑 Warning 🛑)
+    print("# END OF CHECK - 2")
 
     # errs.append(
     #     checkData(
--- proj/custom/vertebrate_custom.py
+++ proj/custom/vertebrate_custom.py
@@ -94,11 +94,7 @@ def vertebrate(all_dfs):
             vertebrateobservation[((vertebrateobservation['no_observation'] == "F") & (vertebrateobservation['taxon'] == 'Not Recorded'))].tmp_row.tolist(),
             'taxon',
             'undefined error',
-<<<<<<< HEAD
-            'If no_observation = F then taxon fields is required, taxon cant be Not Recorded. For more information, you may refer to the <a href="scraper?action=help&layer=lu_vertebratetaxon" target="_blank">lu_vertebratetaxon</a>.'
-=======
             'If no_observation = F then taxon fields is required, taxon cant be Not Recorded. For more information, you may refer to the  <a href=scraper?action=help&layer=lu_vertebratetaxon>lu_vertebratetaxon</a> '
->>>>>>> 296641c1f05baa1ae04ac4d3cfd8e6d4799f5100
             )
     )
 
@@ -109,11 +105,7 @@ def vertebrate(all_dfs):
             vertebrateobservation[((vertebrateobservation['no_observation'] == "F") & (vertebrateobservation['lifestage'] == 'Not Recorded'))].tmp_row.tolist(),
             'lifestage',
             'undefined error',
-<<<<<<< HEAD
-            'If no_observation = F then lifestage fields is required, lifestage cant be Not Recorded.For more information, you may refer to the  <a href="scraper?action=help&layer=lu_vertebratetaxon" target="_blank">lu_vertebratelifestage</a> '
-=======
             'If no_observation = F then lifestage fields is required, lifestage cant be Not Recorded.For more information, you may refer to the  <a href=scraper?action=help&layer=lu_vertebratetaxon>lu_vertebratelifestage</a> '
->>>>>>> 296641c1f05baa1ae04ac4d3cfd8e6d4799f5100
             )
     )
 
@@ -124,11 +116,7 @@ def vertebrate(all_dfs):
             vertebrateobservation[((vertebrateobservation['no_observation'] == "F") & (vertebrateobservation['abundance'] == 'Not Recorded'))].tmp_row.tolist(),
             'abundance',
             'undefined error',
-<<<<<<< HEAD
-            'If no_observation = F then abundance fields is required, abundance cant be Not Recorded.For more information, you may refer to the  <a href="scraper?action=help&layer=lu_vertebrabundance" target="_blank">lu_vertebrateabundance</a> '
-=======
             'If no_observation = F then abundance fields is required, abundance cant be Not Recorded.For more information, you may refer to the  <a href=scraper?action=help&layer=lu_vertebrabundance>lu_vertebrateabundance</a> '
->>>>>>> 296641c1f05baa1ae04ac4d3cfd8e6d4799f5100
             )
     )
    # END OF CHECK - If No_Observation is False, then Taxon is required with value from lu_vertebratetaxon. (🛑 ERROR 🛑)
