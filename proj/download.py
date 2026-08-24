c13095237cf98b1d3bcede4f21df7405c14ac9ee
duynguyen2019 <duynguyenms2020@gmail.com>
2026-08-24 21:49:30 +0000

    fixed an issue with Delineation Checking Tool

--- proj/download.py
+++ proj/download.py
@@ -203,10 +203,10 @@ def get_masterid():
         sites_gdf.crs = "EPSG:4326"
         for column in sites_gdf.select_dtypes(include=['datetime']):
             sites_gdf[column] = sites_gdf[column].astype(str)
-        sites_gdf.rename(columns={'stationcode': 'StationCod', 'new_lat': 'New_Lat', 'new_long': 'New_Long' }, inplace=True)
         print(sites_gdf.columns)
-        sites_gdf.drop(columns=['shape']).to_file(os.path.join(os.getcwd(), "export", "shapefiles_for_download", "sites.shp"))
         sites_gdf.to_file(os.path.join(os.getcwd(), "export", "shapefiles_geojson", "sites.json"), driver='GeoJSON')
+        sites_gdf.rename(columns={'stationcode': 'StationCod', 'new_lat': 'New_Lat', 'new_long': 'New_Long' }, inplace=True)
+        sites_gdf.drop(columns=['shape']).to_file(os.path.join(os.getcwd(), "export", "shapefiles_for_download", "sites.shp"))
 
         # Write catchments to shapefile and geojson
         catchments_gdf = gpd.GeoDataFrame(catchments_df, geometry='geometry')
